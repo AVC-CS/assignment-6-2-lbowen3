3ca688bb1069c2731560a60dcd897a7cd1016c21
lbowen3 <144710267+lbowen3@users.noreply.github.com>
2024-03-13 17:31:10 +0000

    Incomplete code

--- main.hpp
+++ main.hpp
@@ -16,10 +16,18 @@ int getRandom(void)
         /***************************************************
          * Code your program here
          ***************************************************/
+
+        int randnum = rand() % 99;
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
         /***************************************************
          * Code your program here
          ***************************************************/
+
+        int med = (rdnum1 + rdnum2 + rdnum3)/3;
+
+        rdnum1 = rdnum1 - med;
+        rdnum2 = rdnum2 - med;
+        rdnum3 = rdnum3 - med;
 }
