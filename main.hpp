86be96cf1fea4626658f20e40c06fd341875036d
lbowen3 <144710267+lbowen3@users.noreply.github.com>
2024-03-13 18:18:53 +0000

    Edited Code

--- main.hpp
+++ main.hpp
@@ -26,32 +26,17 @@ int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
          ***************************************************/
 
         int med = (rdnum1 + rdnum2 + rdnum3)/3;
-        int i;
-
-        rdnum1 -= med;
-        rdnum2 -= med;
-        rdnum3 -= med;
-
-        if (rdnum1 < 0){
-                rdnum1 *= -1;
-        }
-        if (rdnum2 < 0){
-                rdnum2 *= -1;
-        }
-        if (rdnum3 < 0){
-                rdnum3 *= -1;
-        }
+        int returnvar1 = med;
+        int returnvar2 = med;
 
         while (true){
-                i++;
-                if (i == rdnum1){
-                        return 1;
+                returnvar1++;
+                returnvar2--;
+                if ((returnvar1 == rdnum1)||(returnvar1 == rdnum2)||(returnvar1 == rdnum3)){
+                        return returnvar1;
                 }
-                if (i == rdnum2){
-                        return 2;
-                }
-                if (i == rdnum3){
-                        return 3;
+                if ((returnvar2 == rdnum1)||(returnvar2 == rdnum2)||(returnvar2 == rdnum3)){
+                        return returnvar2;
                 }
         }
 
